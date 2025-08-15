8a3fb40d53dda06e2a197ca11e82e506327377f6
Martin Guevara <mgl.0589@gmail.com>
2025-08-14 19:45:15 -0600

    Modifiying function that extract data from xml files, two functions created from the initial one

--- etl/extract_invoice_generals.py
+++ etl/extract_invoice_generals.py
@@ -2,7 +2,8 @@ import os
 import shutil
 import sys
 import xml.etree.ElementTree as ET
-from path_generator import PathGenerator
+from datetime import datetime
+from path_generator import PathDefiner
 
 # define global variables
 XML_SUFFIX = ".xml"
@@ -10,7 +11,15 @@ PDF_SUFFIX = ".pdf"
 NS = {"cfdi": "http://www.sat.gob.mx/cfd/4"}
 
 # define "year" as argument provided when execution of script
-YEAR = sys.argv[1]
+
+def extract_year():
+    """
+    Extract year either from datetime method or passed as argument
+    """
+    if len(sys.argv) == 1:
+        return sys.argv[1]
+    else:
+        return str(datetime.now().year)
 
 
 def create_directories(directory: str) -> None:
@@ -20,8 +29,11 @@ def create_directories(directory: str) -> None:
     Args:
         directory (str): directory path to be created if not exist
     """
-    if not os.path.exists(directory):
-        os.makedirs(directory, exist_ok=True)
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory, exist_ok=True)
+    except OSError as e:
+        print(f"Directory not found: {e}")
 
 
 def get_files_list(source_directory: str) -> list[str]:
@@ -62,13 +74,16 @@ def change_file_name(source_directory: str, old_name: str, new_name: str) -> Non
     os.rename(old_name, new_name)
 
 
-def extract_invoice_generals(source_directory: str, file_name: str) -> dict[str: str]:
+def extract_invoice_generals(source_directory: str, file_name: str, xml_suffix: str, pdf_suffix: str, ns: str) -> dict[str: str]:
     """
     Extract the general information from each xml file
     
     Args:
-        source_path (str): source directory path from where files will be extracted
+        source_directory (str): source directory path from where files will be extracted
         file_name (str): current name of file that will be read
+        xml_suffix (str): 
+        pdf_suffix (str):
+        ns (str):
     """
     try:
         xml_tree = ET.parse(f"{source_directory}/{file_name}")
@@ -79,39 +94,73 @@ def extract_invoice_generals(source_directory: str, file_name: str) -> dict[str:
     # getting general invoice data
     generals = root.attrib
     date = generals["Fecha"]
-    currency = generals["Moneda"]
     transaction_type = generals["TipoDeComprobante"]
-    subtotal = generals["SubTotal"]
-    total = generals["Total"]
+
     try:
         invoice_id = generals["Folio"]
     except KeyError:
         invoice_id = f"SF-{date.replace("-", "")}"
     
     # getting transmitter data
-    generals_transmitter = root.find("cfdi:Emisor", NS)
+    generals_transmitter = root.find("cfdi:Emisor", ns)
     transmitter_id = generals_transmitter.attrib["Rfc"]
-    transmitter_name = generals_transmitter.attrib["Nombre"]
-
-    # getting receiver data
-    generals_receiver = root.find("cfdi:Receptor", NS)
-    receiver_id = generals_receiver.attrib["Rfc"]
-    # receiver_name = generals_receiver.attrib["Nombre"]
 
     # return dictionary with general information
     return {
         "source_xml_name": f"{file_name}",
-        "source_pdf_name": f"{file_name.split(XML_SUFFIX)[0]}{PDF_SUFFIX}",
-        "new_base_name": f"{date.split("T")[0]}_{transmitter_id}_{invoice_id}_{transaction_type}",
+        "source_pdf_name": f"{file_name.split(xml_suffix)[0]}{pdf_suffix}",
+        "new_base_name": f"{date.split("T")[0]}_{transmitter_id}_{invoice_id}_{transaction_type}", 
+    }
+
+
+def extract_invoice_summary(source_directory: str, file_name: str, ns: str) -> dict[str: str]:
+    """
+    Extract the summary information from each xml file
+    
+    Args:
+        source_path (str): source directory path from where files will be extracted
+        file_name (str): current name of file that will be read
+        ns (str): 
+    """
+    try:
+        xml_tree = ET.parse(f"{source_directory}/{file_name}")
+        root = xml_tree.getroot()
+    except FileNotFoundError as e:
+        print(f"File not found error: {e}")
+    
+    # getting summary invoice data
+    generals = root.attrib
+    date = generals["Fecha"]
+    currency = generals["Moneda"]
+    subtotal = generals["SubTotal"]
+    total = generals["Total"]
+
+    try:
+        invoice_id = generals["Folio"]
+    except KeyError:
+        invoice_id = None
+
+    # getting transmitter data
+    generals_transmitter = root.find("cfdi:Emisor", ns)
+    transmitter_id = generals_transmitter.attrib["Rfc"]
+    transmitter_name = generals_transmitter.attrib["Nombre"]
+
+    # getting receiver data
+    generals_receiver = root.find("cfdi:Receptor", ns)
+    receiver_id = generals_receiver.attrib["Rfc"]
+    receiver_name = generals_receiver.attrib["Nombre"]
+
+    return {
+        "source_xml_name": f"{file_name}",
         "date": date,
         "invoice_id": invoice_id,
         "currency": currency,
-        "transaction": transaction_type,
         "subtotal": subtotal,
         "total": total,
         "transmitter_id": transmitter_id,
         "transmitter_name": transmitter_name,
-        "receiver_id": receiver_id 
+        "receiver_id": receiver_id,
+        "receiver_name": receiver_name
     }
 
 
@@ -152,15 +201,119 @@ def remove_files(source_path:str, file: str) -> None:
 
 
 
-########################### testing ###########################
-print(f"\n{sys.executable}\n")
+########################### main ###########################
+def main():
 
+    print(f"\nExecutable: {sys.executable}\n")
+
+    # extract year
+    year = extract_year()
+
+    # create folders if not exist
+    directory = PathDefiner(year)
+
+    list_directories = [
+                        directory.source_path, 
+                        directory.target_invoice_xml_path, 
+                        directory.target_invoice_pdf_path, 
+                        directory.target_voucher_xml_path, 
+                        directory.target_voucher_pdf_path
+                    ]
+
+
+    # creation of directories recursively
+    print(f"\nCreation of source and target directories started ...\n")
+
+    for directory_name in list_directories:
+        create_directories(directory_name)
+
+    print(f"Source and target directories created successfully!\n")
+
+
+    # extract the file names in source directory
+    original_names_list = get_files_list(directory.source_path)
+    # print(f"{original_names_list}\n")
+
+
+    # remove "." characters if file name has more than 1
+    validated_names_list = [*map(validate_file_name, original_names_list)]
+    # print(f"{validated_names_list}\n")
+
+
+    # combine the original and modified names in a list of tuples
+    names_combined_list = [(old_name, new_name) for old_name, new_name in zip(original_names_list, validated_names_list) if old_name != new_name]
+    # print(f"{names_combined_list}\n")
+
+
+    # change the name of files that has been validated to remove extra "." characters
+    for old_name, new_name in names_combined_list:
+        # change file name if old_name and new name are not equal due to extra-dot-validation
+        if old_name != new_name:
+            try:
+                # print(f"old_name: {old_name}, new_name: {new_name}")
+                change_file_name(directory.source_path, old_name, new_name)
+            except FileExistsError as e:
+                print(f"File not found: {e}")
+
+    invoice_names_list = get_files_list(directory.source_path)
+    # print(invoice_names_list)
+
+
+    # removing pdf files from list
+    xml_invoice_list = [invoice for invoice in invoice_names_list if invoice.endswith(XML_SUFFIX)]
+
+
+    # extract the general information from each xml invoice file
+    print("\nExtracting general data from invoices ...\n")
+
+    invoice_generals_list = [extract_invoice_generals(directory.source_path, invoice, XML_SUFFIX, PDF_SUFFIX, NS) for invoice in xml_invoice_list]
+    # print(invoice_generals_list)
+    # print(f"\nsource xml name: {invoice_generals_list[0]["source_xml_name"]} \nsource pdf name: {invoice_generals_list[0]["source_pdf_name"]} \nnew base name: {invoice_generals_list[0]["new_base_name"]}")
+
+    print("Generals data extracted successfully!\n")
+
+
+    # copy files to respective target folder
+    print(f"\nCopying files from source directory to respective target directory ...\n")
+    for file in invoice_generals_list:
+        
+        if file["new_base_name"].endswith("_I"):
+            xml_target = directory.target_invoice_xml_path
+            pdf_target = directory.target_invoice_pdf_path
+            
+            copy_files(directory.source_path, xml_target, file["source_xml_name"], file["new_base_name"], XML_SUFFIX)
+            copy_files(directory.source_path, pdf_target, file["source_pdf_name"], file["new_base_name"], PDF_SUFFIX)
+
+        
+        elif file["new_base_name"].endswith("_P"):
+            xml_target = directory.target_voucher_xml_path
+            pdf_target = directory.target_voucher_pdf_path
+
+            copy_files(directory.source_path, xml_target, file["source_xml_name"], file["new_base_name"], XML_SUFFIX)
+            copy_files(directory.source_path, pdf_target, file["source_pdf_name"], file["new_base_name"], PDF_SUFFIX)
+            
+    print(f"All files copied successfully!\n")
+    
+
+    # removing files from source directory
+    print(f"\nRemoving files from source directory ...")
+
+    for file_name in original_names_list: 
+        remove_files(directory.source_path, file_name)
+
+    print(f"\nAll files removed successfully!")
+
+
+# main()
+
+########################### testing ###########################
+print(f"\nExecutable: {sys.executable}\n")
+
+# extract year
+year = extract_year()
 
 # create folders if not exist
-directory = PathGenerator(YEAR)
-# print(directory.source_path)
-# print(directory.target_xml_path)
-# print(directory.target_pdf_path)
+directory = PathDefiner(year)
 
 list_directories = [
                     directory.source_path, 
@@ -168,19 +321,16 @@ list_directories = [
                     directory.target_invoice_pdf_path, 
                     directory.target_voucher_xml_path, 
                     directory.target_voucher_pdf_path
-                   ]
+                ]
 
 
 # creation of directories recursively
-print(f"creation of source and target directories started ...\n")
+print(f"\nCreation of source and target directories started ...\n")
 
 for directory_name in list_directories:
-    try:
-        create_directories(directory_name)
-    except FileNotFoundError as e:
-        print(f"Directory not found: {e}")
+    create_directories(directory_name)
 
-print(f"source and target directories created successfully!\n")
+print(f"Source and target directories created successfully!\n")
 
 
 # extract the file names in source directory
@@ -219,11 +369,11 @@ xml_invoice_list = [invoice for invoice in invoice_names_list if invoice.endswit
 # extract the general information from each xml invoice file
 print("\nExtracting general data from invoices ...\n")
 
-invoice_generals_list = [extract_invoice_generals(directory.source_path, invoice) for invoice in xml_invoice_list]
+invoice_generals_list = [extract_invoice_generals(directory.source_path, invoice, XML_SUFFIX, PDF_SUFFIX, NS) for invoice in xml_invoice_list]
 # print(invoice_generals_list)
 # print(f"\nsource xml name: {invoice_generals_list[0]["source_xml_name"]} \nsource pdf name: {invoice_generals_list[0]["source_pdf_name"]} \nnew base name: {invoice_generals_list[0]["new_base_name"]}")
 
-print("\nGenerals data extracted successfully!\n")
+print("Generals data extracted successfully!\n")
 
 
 # copy files to respective target folder
@@ -231,11 +381,6 @@ print(f"\nCopying files from source directory to respective target directory ...
 for file in invoice_generals_list:
     
     if file["new_base_name"].endswith("_I"):
-        # print(f"{directory.source_path}/{file["source_xml_name"]}") 
-        # print(f"{directory.source_path}/{file["source_pdf_name"]}")
-        # print(f"{xml_target}/{file["new_base_name"]}{XML_SUFFIX}") 
-        # print(f"{pdf_target}/{file["new_base_name"]}{PDF_SUFFIX}")
-
         xml_target = directory.target_invoice_xml_path
         pdf_target = directory.target_invoice_pdf_path
         
@@ -244,24 +389,19 @@ for file in invoice_generals_list:
 
     
     elif file["new_base_name"].endswith("_P"):
-        # print(f"{directory.source_path}/{file["source_xml_name"]}") 
-        # print(f"{directory.source_path}/{file["source_pdf_name"]}")
-        # print(f"{xml_target}/{file["new_base_name"]}{XML_SUFFIX}") 
-        # print(f"{pdf_target}/{file["new_base_name"]}{PDF_SUFFIX}")
-
         xml_target = directory.target_voucher_xml_path
         pdf_target = directory.target_voucher_pdf_path
 
         copy_files(directory.source_path, xml_target, file["source_xml_name"], file["new_base_name"], XML_SUFFIX)
         copy_files(directory.source_path, pdf_target, file["source_pdf_name"], file["new_base_name"], PDF_SUFFIX)
         
-print(f"\nAll files copied successfully!\n")
- 
+print(f"All files copied successfully!\n")
+
 
 # removing files from source directory
 print(f"\nRemoving files from source directory ...")
 
-for file_name in original_names_list:
+for file_name in original_names_list: 
     remove_files(directory.source_path, file_name)
 
 print(f"\nAll files removed successfully!")
