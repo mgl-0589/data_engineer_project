536feaa2046f546db83266220f7b203c71eac47c
Martin Guevara <mgl.0589@gmail.com>
2025-09-17 19:28:12 -0600

    Changing path directories in bash script

--- move_files_before_etl_process.sh
+++ move_files_before_etl_process.sh
@@ -2,15 +2,15 @@
 
 YEAR=$(date +%Y%m%d | cut -c1-4)
 
-count_invoices=$(ls -l /mnt/c/Users/ingmg/Documents/{*.xml,*.pdf,*.zip} | wc -l)
-count_sales=$(ls -l /mnt/c/Users/ingmg/Documents/*.xls | wc -l)
+count_invoices=$(ls -l ${LOCAL_DIRECTORY}/{*.xml,*.pdf,*.zip} | wc -l)
+count_sales=$(ls -l ${LOCAL_DIRECTORY}/*.xls | wc -l)
 
 
 if [ $count_invoices -gt 0 ]; then
     echo "moving $count_invoices invoice files ..."
     
     # moving .xml, .pdf, and .zip files into source_files/invoices directory
-    cp /mnt/c/Users/ingmg/Documents/{*.xml,*.pdf,*.zip} /home/mgl/repos/coffee_shop_data_project/source_files/invoices
+    cp ${LOCAL_DIRECTORY}/{*.xml,*.pdf,*.zip} /home/mgl/repos/coffee_shop_data_project/source_files/invoices
 
 fi
 
@@ -18,7 +18,7 @@ if [ $count_sales -gt 0 ]; then
     echo "moving $count_sales sale files ..."
 
     # moving .xls files into source_files/sales directory
-    cp /mnt/c/Users/ingmg/Documents/*.xls /home/mgl/repos/coffee_shop_data_project/source_files/sales
+    cp ${LOCAL_DIRECTORY}/*.xls /home/mgl/repos/coffee_shop_data_project/source_files/sales
 fi
 
 echo $YEAR
